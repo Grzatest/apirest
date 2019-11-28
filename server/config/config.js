6a8f43b6dcb9bd907fd64f8f17fdc50ea4f4030a
Natanael Acero <nataacero1@gmail.com>
2019-11-28 07:44:02 -0600

    Se agrego la conexion a M

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,13 @@
-
 //PUERTO
 process.env.PORT = process.env.PORT || 3000;
 //Entorno
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //Conecciona  a la base de datos
 let urlDB;
-if(process.env.NODE_ENV === 'dev'){
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafeteria';
-}else{
-    urlDB = 'mongodb+srv://admin:admin01@cluster0-rzrls.mongodb.net/cafeteria'
+} else {
+    urlDB = 'mongodb+srv://root:xOMM1EIcaGrKRuxf@cluster0-y1g0q.mongodb.net/cafeteria'
 }
 process.env.URLDB = urlDB;
 
